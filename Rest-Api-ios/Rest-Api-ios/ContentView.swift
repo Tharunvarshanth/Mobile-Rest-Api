e9d00a517b63f647ab24b2faa07b9c727497418b
Tharun <49119576+Tharunvarshanth@users.noreply.github.com>
2020-11-18 17:25:23 +0530

    Add files via upload

---
+++ Rest-Api-ios/Rest-Api-ios/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Rest-Api-ios
+//
+//  Created by user163814 on 11/18/20.
+//  Copyright © 2020 user163814. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
